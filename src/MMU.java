e2115519bb3e86d6563bce9a5fff0f6a53f6a681
CnrHachi <814069356@qq.com>
2020-01-09 16:26:49 +0800

    增加了段页式的地址转换计算

--- src/MMU.java
+++ src/MMU.java
@@ -17,16 +17,17 @@ public class MMU {
                     "1100", "1101", "1110", "1111"};
 
 
-    public static int Page(PCB process, String hexString) {
+    public static int Page(PCB process, String hexAddr) {
         binaryString = "";
-        String[] hexArray = hexString.split("");
+        String[] hexArray = hexAddr.split("");
         for (String hex :
                 hexArray) {
             binaryString += binaryArray[Integer.parseInt(hex, 16)];
         }
 
         if (binaryString.length() < Memory.getMemoryBits()) {
-            for (int i = 0; i < Memory.getMemoryBits() - binaryString.length(); i++) {
+            int length = binaryString.length();
+            for (int i = 0; i < Memory.getMemoryBits() - length; i++) {
                 binaryString = "0" + binaryString;
             }
         }
@@ -54,7 +55,51 @@ public class MMU {
 
     }
 
-    public static void SegmentPage() {
+    public static int SegmentPage(PCB process, List<Integer> addr) {
+        binaryString = "";
+        String segmentNo = Integer.toBinaryString(addr.get(0));
+        String pageNo = Integer.toBinaryString(addr.get(1));
+
+        if (addr.get(0) >= process.SegmentTable.size()) {
+            printError("段越界中断");
+            return -1;
+        }
+
+//        binaryString += segmentNo;
+//        if (binaryString.length() < process.getSegmentBits()) {
+//            for (int i = 0; i < process.getSegmentBits() - binaryString.length(); i++) {
+//                binaryString = "0" + binaryString;
+//            }
+//        }
+
+        if (addr.get(1) >= process.SegmentTable.get(addr.get(0)).pages.size()) {
+            printError("页越界中断");
+            return -1;
+        }
+
+        if (addr.get(2) >= Memory.getPageSize()) {
+            printError("偏移地址越界中断");
+            return -1;
+        }
+
+        binaryString = Integer.toBinaryString(process.SegmentTable.get(addr.get(0)).pages.get(addr.get(1)).getPageFrameNum());
+        if (binaryString.length() < Memory.getPageBits()) {
+            int length = binaryString.length();
+            for (int i = 0; i < Memory.getPageBits() - length; i++) {
+                binaryString = "0" + binaryString;
+            }
+        }
+
+        String temp = Integer.toBinaryString(addr.get(2));
+        if (temp.length() < Memory.getMemoryBits() - Memory.getPageBits()) {
+            int length = temp.length();
+            for (int i = 0; i < Memory.getMemoryBits() - Memory.getPageBits() - length; i++) {
+                temp = "0" + temp;
+            }
+        }
+        binaryString += temp;
+        printResult(Integer.toHexString(Integer.parseInt(binaryString, 2)));
+        return 1;
 
     }
 
@@ -63,7 +108,7 @@ public class MMU {
     }
 
     private static void printError(String string) {
-        System.out.println(string);
+        System.out.println(">ERROR: " + string);
     }
 
 }
