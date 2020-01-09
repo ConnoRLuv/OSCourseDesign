e7d17abbd3327dc9690220d8f536f25ac3781b1b
CnrHachi <814069356@qq.com>
2020-01-09 16:26:20 +0800

    模拟段页式 段的数据结构

--- src/Segment.java
+++ src/Segment.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Segment {
+    private String segmentNam;
+    private int segmentNum;
+    private int segmentSize;
+    List<Page> pages;
+
+    public Segment(String segmentNam, int segmentSize) {
+        this.segmentNam = segmentNam;
+        this.segmentSize = segmentSize;
+        pages = new ArrayList<>();
+    }
+
+    public int getSegmentSize() {
+        return segmentSize;
+    }
+
+    public void setSegmentSize(int segmentSize) {
+        this.segmentSize = segmentSize;
+    }
+
+    public String getSegmentNam() {
+        return segmentNam;
+    }
+
+    public void setSegmentNam(String segmentNam) {
+        this.segmentNam = segmentNam;
+    }
+
+    public int getSegmentNum() {
+        return segmentNum;
+    }
+
+    public void setSegmentNum(int segmentNum) {
+        this.segmentNum = segmentNum;
+    }
 }
